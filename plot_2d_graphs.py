0cdedf87f1a04b0509ac5c399f47fde82e0339c0
Gbolahan <105363962+Superboy-GB@users.noreply.github.com>
2023-10-30 00:23:27 +0100

    Update plot_2d_graphs.py

--- plot_2d_graphs.py
+++ plot_2d_graphs.py
@@ -7,3 +7,12 @@ color='blue')
 
 # Show the plot
 p.show()
+
+         # Define the mathematical function or expression you want to plot
+f(x) = (15-3*x)/3 - (7*x-14)/3
+
+# Create a plot of the function in the range a to b
+p = plot(f, (x, -1, 10), title='Simple Plot', legend_label= 'Graph of (15-3*x)/3 - (7*x-14)/3', color='blue')
+
+# Show the plot
+p.show()
