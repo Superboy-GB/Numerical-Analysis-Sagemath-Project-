6258e7ea815c04b82f406e628e5e3d90c68c6ddd
Gbolahan <105363962+Superboy-GB@users.noreply.github.com>
2023-10-29 18:17:28 +0100

    Add files via upload

---
+++ plot_2d_graphs.py
@@ -0,0 +1,9 @@
+# Define the mathematical function or expression you want to plot
+f(x) = # Your expression
+
+# Create a plot of the function in the range a to b
+p = plot(f, (x, a, b), title='Simple Plot', legend_label= (# Your function in the next hyphens)'',
+color='blue')
+
+# Show the plot
+p.show()
